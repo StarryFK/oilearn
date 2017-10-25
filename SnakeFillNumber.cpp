65583691f12791c12633662aefd08dfd525f16e3
StarryFK <starryfk@hotmail.com>
2017-10-25 17:30:28 +0800

    Snakeproblem finish

--- SnakeFillNumber.cpp
+++ SnakeFillNumber.cpp
@@ -3,14 +3,27 @@
 #define MAXN 99
 
 int a[MAXN][MAXN];
-memset(a, 0, sizeof(a));
 
 int main()
 {
-	int n, x=0, y=0;
+	memset(a, 0, sizeof(a));
+	int n, now, x, y;
 	scanf("%d", &n);
-	a[0][n-1] = int now = 1;
-	while( now <= n*n)
+	a[x=0][y=n-1] = now = 1;
+	while( now < n*n)
 	{
-		while(y+1<n && !a[x][y+1]) a[x][++y] = now++;
-		while(
+		while(x+1<n && !a[x+1][y]) a[++x][y] = ++now;
+		while(y-1>=0 && !a[x][y-1]) a[x][--y] = ++now;
+		while(x-1>=0 && !a[x-1][y]) a[--x][y] = ++now;
+		while(y+1<n && !a[x][y+1]) a[x][++y] = ++now;
+	}
+	for(int i=0; i<n; i++)
+	{
+		for(int j=0; j<n; j++)
+		{
+			printf("%3d", a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
