96982ec7733cfafd7806d4185460783142af41af
StarryFK <starryfk@hotmail.com>
2017-12-25 18:48:50 +0800

    editing fileI/O

--- c/test.c
+++ c/test.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define LEN sizeof(Stud)
 
+FILE * in;
+FILE * out;
 
-FILE * fi;
-FILE * fo;
+struct Student
+{
+	int num;
+	char name[20];
+	float score;
+	struct Student *next;
+};
+
+typedef struct Student Stud;
+
+int num;
+
+Stud * create(FILE * in)
+{
+	Stud *head=null, *p1, *p2;
+	p1 = p2 = malloc(LEN);
+	if(p1 == null)
+	{
+		printf("Fail to malloc\n");
+		return;
+	}
+	fscanf(in
 
 int main()
 {
-	if((fi=fopen("input.txt", "r")) == NULL)
+	if((in=fopen("input.txt", "r")) == NULL)
 	{
 		printf("Cannot open the input file");
 		exit(0);
-
 	}
-	fo = fopen("output.txt", "w");
+	if((out=fopen("output.txt", "w")) == NULL)
+	{
+		printf("Cannot open the output file");
+		exit(0);
+	}
 
 	fclose(fi);
 	fclose(fo);
